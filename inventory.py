54de9fa202d17db8b0b96228194612bb58c693f3
D. Daniel Iramain <danieliramain97@gmail.com>
2024-08-01 09:30:07 -0300

    Metodos de validacion (clase base)

--- inventory.py
+++ inventory.py
@@ -11,14 +11,18 @@ Objetivo: Desarrollar un sistema para manejar productos en un inventario.
 '''
 
 class Producto():
-    def __init__(self, nombre, costo, precio, cantidad) -> None:
+    def __init__(self, codigo, nombre, costo, precio, cantidad) -> None:
+        self.__codigo = codigo
         self.__nombre = nombre
-        self.__costo = costo
-        self.__precio = precio
-        self.__cantidad = cantidad
+        self.__costo = self.validar_costo(costo)
+        self.__precio = self.validar_precio(precio)
+        self.__cantidad = self.validar_cantidad(cantidad)
 
-    
     #Getters
+    @property
+    def codigo(self):
+        return self.__codigo
+
     @property
     def nombre(self):
         return self.__nombre.capitalize()
@@ -36,6 +40,10 @@ class Producto():
         return self.__cantidad
     
     #Setters
+    @codigo.setter
+    def codigo(self):
+        pass
+
     @nombre.setter
     def nombre(self):
         '''
@@ -54,3 +62,45 @@ class Producto():
     @cantidad.setter
     def cantidad(self, nueva_cantidad):
         self.__cantidad = self.validar_cantidad(nueva_cantidad)
+
+    def validar_costo(self, costo):
+        try:
+            costo_num = float(costo)
+            if costo_num <= 0:
+                raise ValueError('El costo del producto no puede ser negativo o cero')
+            return costo_num
+        except ValueError:
+            print('El costo debe ser numérico')
+
+    def validar_precio(self, precio):
+        try:
+            precio_num = float(precio)
+            if precio_num <= 0:
+                raise ValueError('El precio del producto no puede ser negativo o cero')
+            return precio_num
+        except ValueError:
+            print('El precio debe ser numérico')
+        
+    def validar_cantidad(self, cantidad):
+        try:
+            cantidad_num = int(cantidad)
+            if cantidad < 0:
+                raise ValueError('La cantidad no puede ser menor a 0')
+            return cantidad_num
+        except ValueError:
+            print('La cantidad debe ser un número entero')
+    
+    def to_dict(self):
+        '''
+        método para devolver un dicc porque vamos a trasladarlo a JSON
+        '''
+        return {
+            'codigo': self.codigo,
+            'nombre': self.nombre,
+            'costo': self.costo,
+            'precio': self.precio,
+            'cantidad': self.cantidad
+        }
+
+    def __str__(self) -> str:
+        return f'{self.codigo} {self.nombre}'
